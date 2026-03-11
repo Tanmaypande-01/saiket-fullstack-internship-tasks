a2565d00d4f89f977264bb745ff3677f5a7d24b0
Tanmaypande-01 <tanmaypande321@gmail.com>
2026-03-11 21:46:47 +0530

    Add files via upload

---
+++ Task-5/routes/userRoutes.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const {
+  createUser,
+  getAllUsers,
+  getUserById,
+  updateUser,
+  deleteUser
+} = require("../controllers/userController");
+
+
+const router = express.Router();
+
+router.route("/").post(createUser).get(getAllUsers);
+
+router.route("/:id").get(getUserById).put(updateUser).delete(deleteUser);
+
+module.exports = router;
+
